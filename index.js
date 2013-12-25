1f3084b00ebc94a3d9b2af50697b84d193133e43
kaelzhang <i@kael.me>
2013-12-25 15:31:52 +0800

    update plugins

--- index.js
+++ index.js
@@ -2,6 +2,16 @@
 
 var Switch = module.exports = require('./lib/switch');
 
+// TODO:
+// use plugins as entries ?
 var Switch.Plugins = {
-    
+    accordion   : require('./plugins/accordion'),
+    autoplay    : require('./plugins/autoplay'),
+    carousel    : require('./plugins/carousel'),
+    cleaner     : require('./plugins/cleaner'),
+    endless     : require('./plugins/endless'),
+    fade        : require('./plugins/fade'),
+    lazyload    : require('./plugins/lazyload'),
+    step        : require('./plugins/step'),
+    tabswitch   : require('./plugins/tabswitch')
 };
