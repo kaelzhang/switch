a814337ea603847e1a6810346b88815b1b503fad
kaelzhang <i@kael.me>
2014-06-17 18:17:25 +0800

    complete plugin system; #1

--- index.js
+++ index.js
@@ -1,18 +1,13 @@
 'use strict';
 
-var Switch = module.exports = require('./lib/switch');
+module.exports = s;
+
+function s() {
+  return new Switch();
+}
+
+s.Switch = require('./lib/switch');
 
 // TODO:
 // use plugins as entries ?
-Switch.Plugins = {
-    accordion   : require('./plugins/accordion'),
-    autoplay    : require('./plugins/autoplay'),
-    carousel    : require('./plugins/carousel'),
-    cleaner     : require('./plugins/cleaner'),
-    endless     : require('./plugins/endless'),
-    fade        : require('./plugins/fade'),
-    lazyload    : require('./plugins/lazyload'),
-    step        : require('./plugins/step'),
-    tabswitch   : require('./plugins/tabswitch')
-};
-
+s.Plugins = require('./lib/plugins');
